f851067ababddba0ac97b1d9fcd35ed9115cfa1a
KimDaJin <kimdajin514@gmail.com>
2024-03-21 16:46:34 +0900

    refactor: di with provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:search_apple_app/data/data_source/pixabay_api.dart';
-import 'package:search_apple_app/data/respository/photo_api_repository_impl.dart';
-import 'package:search_apple_app/domain/use_case/get_photos_use_case.dart';
-import 'package:search_apple_app/trash/photo_provider.dart';
+import 'package:search_apple_app/di/provider_setup.dart';
 import 'package:search_apple_app/presentation/home/home_screen.dart';
-import 'package:search_apple_app/presentation/home/home_viewmodel.dart';
-import 'package:http/http.dart' as http;
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: globalProviders,
+      child: const MyApp(),
+    )
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Clean Architecture Practice App',
       debugShowCheckedModeBanner: false,
       // InheritedWidget 사용
@@ -26,10 +26,12 @@ class MyApp extends StatelessWidget {
       //   child: const HomeScreen()),
 
       // Provider 사용
-      home: ChangeNotifierProvider(
-        create: (_) => HomeViewModel(GetPhotosUseCase(PhotoApiRepositoryImpl(PixabayApi(http.Client())))),
-        child: const HomeScreen(),
-      ),
+      // home: ChangeNotifierProvider(
+      //   create: (_) => HomeViewModel(GetPhotosUseCase(PhotoApiRepositoryImpl(PixabayApi(http.Client())))),
+      //   child: const HomeScreen(),
+      // ),
+
+      home: HomeScreen(),
     );
   }
 }
